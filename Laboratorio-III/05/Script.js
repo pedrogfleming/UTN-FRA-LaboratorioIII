fae30657d877f7fd8fa3ac8848f224eccec18c2c
Geraghty Fleming Pedro <pedrogfleming@gmail.com>
2022-05-02 23:21:52 -0300

    clases 2 a 5

---
+++ Laboratorio-III/05/Script.js
@@ -0,0 +1,12 @@
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+for (const iterator of object) {
+    
+}
+array.forEach(element => {
+    
+});
